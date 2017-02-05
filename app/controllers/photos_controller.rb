f871fc7d778b91b89a7bfd301fc09c1ef2d6b141
DunlapN <DunlapN@users.noreply.github.com>
2017-02-05 00:26:30 -0600

    Final

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,51 @@
 class PhotosController < ApplicationController
+
+  # CREATE
+  def new_form
+
+    render("/photos/new_form.html.erb")
+  end
+
+  def create_row
+    new_photo = Photo.new
+    new_photo.caption = params[:the_caption]
+    new_photo.source = params[:the_source]
+    new_photo.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  # edit
+  def edit_form
+    @photo = Photo.find(params[:id])
+    render ("/photos/edit_form.html.erb")
+  end
+
+  def update_row
+    photo = Photo.find(params[:id])
+    photo.caption = params[:the_caption]
+    photo.source = params[:the_source]
+    photo.save
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  # READ
   def index
     @list_of_photos = Photo.all
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+
+    render("/photos/show.html.erb")
+  end
+
+  #DELETE
+
+  def destroy
+    photo = Photo.find(params[:id])
+    photo.delete
+    redirect_to("http://localhost:3000/photos")
+
   end
 
 
